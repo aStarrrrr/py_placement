aaf998a6eb7d8151e54c0a5764772dbb54e6479e
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-02-01 20:16:19 +0530

    Replacing the characters with other in between alphabets only

--- replace a character by other.py
+++ replace a character by other.py
@@ -1,1 +1,13 @@
-print("hi)
+import re
+
+sample = "Abhinand#$!Shaji$#"
+final = re.sub(r'(?<=\w)[$#!]+(?=\w)', ' ', sample)
+
+print(final)
+
+
+
+# Explanation
+#           (?<=\w): Ensures that the special character is preceded by a word character (A-Z, a-z, 0-9).
+#           [$#!]+: Matches one or more occurrences of only the specified special characters ($, #, !).
+#           (?=\w): Ensures that the special character is followed by a word character.
